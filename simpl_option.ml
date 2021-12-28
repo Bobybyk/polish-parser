53cbd580bd0041d796d2b09e321b0333233bc8c5
JACOTOT Hugo <hjacotot@free.fr>
2021-12-28 16:41:17 +0100

    simpl now calculates operations between constants outside of IF/WHILE blocks

--- simpl_option.ml
+++ simpl_option.ml
@@ -1,8 +1,23 @@
 open Types;;
+open Printf;;
 
-let simplify_expr (e: expr) : expr =
+let is_num (e:expr) : bool = 
         match e with
-        | Op(o,e1,e2) -> failwith("TODO")
+        | Num(i) -> true
+        | _ -> false;;
+
+let plus_num (a:expr) (b:expr) : int =
+        match a with
+        | Num(e1) -> (match b with
+                        | Num(e2) -> (e1 + e2)
+                        | _ -> failwith("Error plus_num b")) 
+        | _ -> failwith("Error plus_num a");;
+
+let rec simplify_expr (e: expr) : expr =
+        match e with
+        | Op(o,e1,e2) -> if (is_num (simplify_expr e1)) && (is_num (simplify_expr e2)) 
+                        then Num((plus_num e1 e2))
+                        else e
         | _ -> e;;
 
 let simplify_aux (p: (position * instr) ) : (position * instr) = 
