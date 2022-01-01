69d82bb59b3871b5857a27d0b513d9e8a119cdb4
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-01-01 22:20:34 +0100

    eval vars conditions et debbug

--- eval_vars.ml
+++ eval_vars.ml
@@ -6,29 +6,29 @@ let add_env name list_var : (name * int) list =
     then (name, 1)::List.remove_assoc name list_var
   else (name, 1)::list_var;; 
 
-let rec eval_type (e:expr) list_var =
+let rec eval_type (e:expr) list_var : (name * int) list =
   match e with
     | Num(i) -> list_var
-    | Var(n) -> if List.mem_assoc n list_var then list_var else printf ("Var "^n^" can be not initialised") && list_var
-    | Op(o, e1, e2) -> eval_type e1 list_var && eval_type e2 list_var
+    | Var(n) -> if List.mem_assoc n list_var then list_var else (print_string ("Var "^n^" can be not initialised") ; list_var)
+    | Op(o, e1, e2) -> eval_type e1 (eval_type e2 list_var)
 
 let check_var_cond (condition:cond) list_var : (name * int) list =
-  match cond with (val1, comp_type, val2) -> list_var
+  match condition with (val1, comp_type, val2) -> eval_type val1 (eval_type val1 list_var)
 
-let rec browse_block (block:block) list_var : (name * int) list =
+let browse_block (block:block) list_var : (name * int) list =
   match block with
     | [] -> list_var
     | instr::program' -> list_var
 
-let rec eval_instr (instr:instr) list_var : (name * int) list =
+let eval_instr (instr:instr) list_var : (name * int) list =
   match instr with 
     | Set(n,e) -> add_env n list_var
     | Read(n) -> add_env n list_var
     | Print(e) -> list_var
-    | If(c, b1, b2) -> browse_block b1 (check_var_cond c list_var) 
+    | If(c, b1, b2) -> (check_var_cond c (browse_block b1 (browse_block b2 list_var)))
     | While(c, d) -> list_var
 
-let rec browse_program (program:program) list_var : unit =
+let rec browse_program_vars (program:program) list_var : unit =
   match program with
     | [] -> ()
-    | instr::program' -> browse_program program' (eval_instr (snd instr) list_var);;
+    | instr::program' -> browse_program_vars program' (eval_instr (snd instr) list_var);;
---
+++ eval_vars.mli
@@ -0,0 +1,3 @@
+open Types;;
+
+val browse_program_vars : program -> (name * int) list -> unit
