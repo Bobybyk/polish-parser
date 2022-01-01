b3a0fb01e79f8d374537f999ab5a19bb0e4be3f7
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-01-01 22:50:03 +0100

    eval_vars every cases evaluated (normalement)

--- eval_vars.ml
+++ eval_vars.ml
@@ -15,18 +15,18 @@ let rec eval_type (e:expr) list_var : (name * int) list =
 let check_var_cond (condition:cond) list_var : (name * int) list =
   match condition with (val1, comp_type, val2) -> eval_type val1 (eval_type val1 list_var)
 
-let browse_block (block:block) list_var : (name * int) list =
+let rec browse_block (block:block) list_var : (name * int) list =
   match block with
     | [] -> list_var
-    | instr::program' -> list_var
+    | instr::program' -> browse_block program' (eval_instr (snd instr) list_var)
 
-let eval_instr (instr:instr) list_var : (name * int) list =
+and eval_instr (instr:instr) list_var : (name * int) list =
   match instr with 
     | Set(n,e) -> add_env n list_var
     | Read(n) -> add_env n list_var
     | Print(e) -> list_var
     | If(c, b1, b2) -> (check_var_cond c (browse_block b1 (browse_block b2 list_var)))
-    | While(c, d) -> list_var
+    | While(c, d) -> (check_var_cond c (browse_block d list_var))
 
 let rec browse_program_vars (program:program) list_var : unit =
   match program with
