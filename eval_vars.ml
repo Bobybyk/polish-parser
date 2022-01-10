15ba99c12e450be8c7baa862ff6060a03a00bd9c
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-01-10 23:01:11 +0100

    eval vars, format conforme

--- eval_vars.ml
+++ eval_vars.ml
@@ -6,37 +6,42 @@ let add_env name list_var : (name * int) list =
     then list_var
   else (name, 1)::list_var;; 
 
-let rec eval_type (e:expr) list_var : (name * int) list =
+(* let rec eval_type (e:expr) list_var list_var_unint =
   match e with
     | Num(i) -> list_var
     | Var(n) -> if List.mem_assoc n list_var then list_var else (print_string ("Var "^n^" can be not initialised\n") ; list_var)
-    | Op(o, e1, e2) -> eval_type e1 (eval_type e2 list_var)
+    | Op(o, e1, e2) -> eval_type e1 (eval_type e2 list_var list_var_unint) list_var_unint *)
 
-let check_var_cond (condition:cond) list_var : (name * int) list =
-  match condition with (val1, comp_type, val2) -> eval_type val1 (eval_type val2 list_var)
-
-let rec eval_instr_under_block (instr:instr) list_var : (name * int) list =
+(* let check_var_cond (condition:cond) list_var list_var_unint =
+  match condition with (val1, comp_type, val2) -> eval_type val1 (eval_type val2 list_var list_var_unint) list_var_unint *)
+ 
+let rec eval_instr_under_block (instr:instr) list_var : ((name * int) list) =
   match instr with 
-    | Set(n,e) -> list_var
-    | Read(n) -> list_var
-    | Print(e) -> eval_type e list_var
-    | If(c, b1, b2) -> (check_var_cond c (browse_block b1 (browse_block b2 list_var)))
-    | While(c, d) -> (check_var_cond c (browse_block d list_var))
+    | Set(n,e) -> add_env n list_var
+    | Read(n) -> add_env n list_var
+    | Print(e) -> list_var
+    | If(c, b1, b2) -> browse_block b1 (browse_block b2 list_var)
+    | While(c, d) -> (browse_block d list_var)
 
-and browse_block (block:block) list_var : (name * int) list =
+and browse_block (block:block) list_var : ((name * int) list) =
   match block with
     | [] -> list_var
     | instr::program' -> browse_block program' (eval_instr_under_block (snd instr) list_var)
 
-let eval_instr (instr:instr) list_var : (name * int) list =
+let eval_instr (instr:instr) list_var list_var_unint : (((name * int) list) * ((name * int) list)) =
   match instr with 
-    | Set(n,e) -> add_env n list_var
-    | Read(n) -> add_env n list_var
-    | Print(e) -> eval_type e list_var
-    | If(c, b1, b2) -> (browse_block b2 (browse_block b1 (check_var_cond c list_var)))
-    | While(c, d) -> (check_var_cond c (browse_block d list_var))
+    | Set(n,e) -> ((add_env n list_var) , (add_env n list_var_unint))
+    | Read(n) -> ((add_env n list_var) , (add_env n list_var_unint))
+    | Print(e) -> (list_var , list_var_unint) 
+    | If(c, b1, b2) -> ((browse_block b2 (browse_block b1 list_var)), list_var_unint)
+    | While(c, d) -> ((browse_block d list_var), list_var_unint)
 
-let rec browse_program_vars (program:program) list_var : unit =
+let rec print_list_assoc list_var : unit =
+  match list_var with
+  | [] -> ()
+  | e::l' -> printf "%s " (fst e) ; print_list_assoc l'
+
+let rec browse_program_vars (program:program) list_var list_var_unint : unit =
   match program with
-    | [] -> ()
-    | instr::program' -> browse_program_vars program' (eval_instr (snd instr) list_var);;
+    | [] -> print_list_assoc list_var ; printf "\n" ; print_list_assoc list_var_unint
+    | instr::program' -> let p = eval_instr (snd instr) list_var list_var_unint in browse_program_vars program' (fst p) (snd p);;
